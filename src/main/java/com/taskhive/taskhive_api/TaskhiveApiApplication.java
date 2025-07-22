eea9cc15f212ac8a3df30e20514ef38d7ca6bbb6
Bryce <brycegevans@gmail.com>
2025-07-22 15:37:22 -0600

    Initial Commit

---
+++ src/main/java/com/taskhive/taskhive_api/TaskhiveApiApplication.java
@@ -0,0 +1,13 @@
+package com.taskhive.taskhive_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskhiveApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskhiveApiApplication.class, args);
+	}
+
+}
